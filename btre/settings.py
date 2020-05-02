682a671e793cbdc9d72718cfa1f7d44b518b0565
leroyg <leroy.gardner@gmail.com>
2020-05-01 21:39:56 -0400

    add dynamic elements to the listings page, and updated url to listing

--- btre/settings.py
+++ btre/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 
-#media folder Settings
+# media folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
